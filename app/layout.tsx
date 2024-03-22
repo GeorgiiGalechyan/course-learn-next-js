ce4f88804196e68e3a3c80d1d4cba72e8dc1b80b
Georgii Galechyan <galechyan1991@gmail.com>
2024-03-22 13:42:13 +0400

    Finished chapter 3

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,6 @@
+import '@/app/ui/global.css';
+import { lusitana700 } from '@/fonts';
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +8,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${lusitana700.className}`}>{children}</body>
     </html>
   );
 }
---
+++ app/ui/fonts.ts
@@ -0,0 +1,10 @@
+import { Inter } from 'next/font/google';
+import { Lusitana } from 'next/font/google';
+
+export const inter = Inter({
+  subsets: ['latin'],
+  weight: ['400', '500', '600', '700'],
+});
+
+export const lusitana400 = Lusitana({ subsets: ['latin'], weight: '400' });
+export const lusitana700 = Lusitana({ subsets: ['latin'], weight: '700' });
