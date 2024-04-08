0352aec0c1f51459af944dce0593ec8a1dc1ff65
Georgii Galechyan <galechyan1991@gmail.com>
2024-04-08 17:21:47 +0400

    Course is finished!

--- app/dashboard/customers/page.jsx
+++
@@ -1,5 +0,0 @@
-// app/dashboard/customers
-
-export default function Page() {
-  return <p>Customers Page</p>;
-}
---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,11 @@
+// app/dashboard/customers
+
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Customers',
+};
+
+export default function Page() {
+  return <p>Customers Page</p>;
+}
